43215795bdcbded698e53523c484465d44cfcbea
tomeklucz <tomasz.kluczynskii@gmail.com>
2022-06-14 18:40:47 +0200

    final results

--- script.js
+++ script.js
@@ -5,9 +5,11 @@ const buttons = document.querySelectorAll(".accordion button");
 const expandContent = function () {
   const buttonStatus = this.getAttribute("aria-expanded");
 
-  buttonStatus === "false"
-    ? this.setAttribute("aria-expanded", true)
-    : this.setAttribute("aria-expanded", false);
+  for (let button of buttons) {
+    button.setAttribute("aria-expanded", false);
+  }
+
+  if (buttonStatus === "false") this.setAttribute("aria-expanded", true);
 };
 
 buttons.forEach((button) => button.addEventListener("click", expandContent));
